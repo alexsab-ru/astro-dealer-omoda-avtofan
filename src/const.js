84a607da00dc3bb790d0ebbc54b1ba17cf0e5176
psdhtmlcss <pochtoviy-yashik@mail.ru>
2025-08-21 11:37:20 +0400

    Update const.js

--- src/const.js
+++ src/const.js
@@ -71,6 +71,8 @@ export const LINKS_MENU = menu;
 export const COLLECTIONS = [
 	{name: 'special-offers', title: 'Спецпредложения', description: `Спецпредложения официального дилерского центра ${site_name} ${brand} ${legal_city}`},
 	{name: 'news', title: 'Новости', description: `Новости и акции дилерского центра ${site_name} ${brand} ${legal_city}`},
+	{name: 'customers-credit', title: 'Кредитование и страхование'},
+	{name: 'owners-support', title: 'Поддержка'},
 ];
 
 // Текст согласия в формах
